221a96345b6d6533d3ee6553989c14ffda75d2b2
Aommykung1250-X7 <phanuwataom1250@gmail.com>
2026-06-14 22:53:39 +0700

    Database Design

--- frontend/.next/static/chunks/pages/_app.js
+++ frontend/.next/static/chunks/pages/_app.js
@@ -1,8 +1,14 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/[root of the server]__f81d50._.js",
-  "static/chunks/node_modules_react-dom_82bb97._.js",
-  "static/chunks/node_modules_beb007._.js",
-  "static/chunks/[root of the server]__f265a1._.js",
-  "static/chunks/pages__app_5771e1._.js",
-  "static/chunks/pages__app_f2320d._.js"
+  "static/chunks/node_modules_next_dist_compiled_next-devtools_index_5277ebc8.js",
+  "static/chunks/node_modules_next_dist_compiled_166120c5._.js",
+  "static/chunks/node_modules_next_dist_shared_lib_c54e23e3._.js",
+  "static/chunks/node_modules_next_dist_client_d0aa886c._.js",
+  "static/chunks/node_modules_next_dist_5b402162._.js",
+  "static/chunks/node_modules_next_app_72f3d36f.js",
+  "static/chunks/[next]_entry_page-loader_ts_742e4b53._.js",
+  "static/chunks/node_modules_react-dom_4411d9bd._.js",
+  "static/chunks/node_modules_db4bb196._.js",
+  "static/chunks/[root-of-the-server]__45f039c3._.js",
+  "static/chunks/pages__app_2da965e7._.js",
+  "static/chunks/turbopack-pages__app_6961bd01._.js"
 ])
